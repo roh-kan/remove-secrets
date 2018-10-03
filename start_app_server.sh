7423ac8b5af4572a2a1c2cf502fd4cc4fe490b0a
roh-kan <rk@Comp.local>
2018-10-03 17:42:16 +0800

    removed secret

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://user1:user1pass@ds121593.mlab.com:21593/mlab-trial
+#export MONGODB_CREDENTIALS=mongodb://user1:user1pass@ds121593.mlab.com:21593/mlab-trial
 
 # finally, run the app in dev mode...
 npm run dev
